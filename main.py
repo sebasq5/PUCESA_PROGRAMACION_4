4f7911f4eda101b613e9d0694ac1597f4efe00cd
sebasq5 <sebasanmartin456@gmail.com>
2025-04-11 12:28:16 -0500

    subir main mi primer reporsitorio

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Es mi primera prueba de Python con GitHub y probando la herramienta de git")
+print("Me llamo Sebastian Sanmartin")
+print("Soy estudiante de la PUCESA")
+
+for i in range (5):
+    print("Ya acabé el ", i+1 ,"semestre.")
