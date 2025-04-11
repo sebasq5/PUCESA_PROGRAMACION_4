cc5ee5ef459b99e04d26ed480eeaa6c5972baf23
sebasq5 <sebasanmartin456@gmail.com>
2025-04-11 13:37:22 -0500

    se elimino el main que se uso solo para configurar el git

--- main.py
+++
@@ -1,6 +0,0 @@
-print("Es mi primera prueba de Python con GitHub y probando la herramienta de git")
-print("Me llamo Sebastian Sanmartin")
-print("Soy estudiante de la PUCESA")
-
-for i in range (5):
-    print("Ya acabé el ", i+1 ,"semestre.")
